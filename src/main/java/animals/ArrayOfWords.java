2a05c5df9a3d85789d5fae11ea998d244cf000e7
Anna-b2 <84246490+Anna-b2@users.noreply.github.com>
2024-06-06 00:13:32 +0300

    Update ArrayOfWords.java

--- src/main/java/animals/ArrayOfWords.java
+++ src/main/java/animals/ArrayOfWords.java
@@ -1,25 +1,20 @@
 package ArrayOfWords;
-
 import java.util.*;
-
 public class ArrayOfWords {
     public static void main(String[] args){
         List<String> fruits = Arrays.asList(
-                "Mercedes", "BMW", "Audi", "Toyota", "Reno",
-                "Opel", "Subaru", "Range Rover", "Toyota", "Reno",
-                "BMW", "Audi", "Toyota", "Lada", "Subaru",
-                "Audi", "Toyota", "Subaru", "Subaru", "Lada"
+                "Апельсин", "Банан", "Персик", "Нектарин", "банан",
+                "Лимон", "Лайм", "Персик", "Киви", "Лайм",
+                "Киви", "Персик", "Киви", "Киви"
         );
-
         Set<String> unique = new HashSet<>(fruits);
-
-        System.out.println("Initial array :");
+        System.out.println("Введенный список:");
         System.out.println(fruits);
         System.out.println();
-        System.out.println("Unique words :");
+        System.out.println("Уникальные наименования:");
         System.out.println(unique);
         System.out.println();
-        System.out.println("The words are repeated :");
+        System.out.println("Повторяющиеся слова:");
         for (String key : unique) {
             System.out.println(key + ": " + Collections.frequency(fruits, key));
         }
