7d1a767814e34508d0555e39ce7921b27071fc2e
Anna-b2 <84246490+Anna-b2@users.noreply.github.com>
2024-06-06 00:14:46 +0300

    Update PhoneBook.java

--- src/main/java/geometric/PhoneBook.java
+++ src/main/java/geometric/PhoneBook.java
@@ -1,14 +1,10 @@
 package PhoneBook;
-
 import java.util.*;
-
 public class PhoneBook {
     private Map<String, List<String>> phoneBook;
-
     public PhoneBook() {
         phoneBook = new HashMap<>();
     }
-
     public void add(String lastName, String phoneNumber) {
         List<String> numbers = phoneBook.get(lastName);
         if (numbers == null) {
@@ -17,7 +13,6 @@ public class PhoneBook {
         numbers.add(phoneNumber);
         phoneBook.put(lastName, numbers);
     }
-
     public List<String> get(String lastName) {
         List<String> numbers = phoneBook.get(lastName);
         if (numbers == null) {
