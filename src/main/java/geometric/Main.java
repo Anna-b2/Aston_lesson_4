7b792c6d2f542f11feb52a7f270ab03165be7f70
Anna-b2 <84246490+Anna-b2@users.noreply.github.com>
2024-06-05 23:54:05 +0300

    Update Main.java

--- src/main/java/geometric/Main.java
+++ src/main/java/geometric/Main.java
@@ -6,28 +6,28 @@ public class Main {
     public static void main(String[] args) {
         PhoneBook phoneBook1 = new PhoneBook();
 
-        phoneBook1.add("Dad", "1111111");
-        phoneBook1.add("Dad", "2222222");
-        phoneBook1.add("Mom", "3333333");
-        phoneBook1.add("Sister", "4444444");
+        phoneBook1.add("Иванов", "292859037");
+        phoneBook1.add("Иванов", "297867777");
+        phoneBook1.add("Петров", "331212122");
+        phoneBook1.add("Малышев", "442345345");
 
-        List<String> numbers1 = phoneBook1.get("Dad");
-        List<String> numbers2 = phoneBook1.get("Mom");
-        List<String> numbers3 = phoneBook1.get("Brother");
+        List<String> numbers1 = phoneBook1.get("Иванов");
+        List<String> numbers2 = phoneBook1.get("Петров");
+        List<String> numbers3 = phoneBook1.get("Малышев");
 
-        System.out.println("Dad's phone numbers:");
+        System.out.println("Номера телефонов для фамилии Иванов:");
         for (String number : numbers1) {
             System.out.println(number);
         }
 
         System.out.println();
-        System.out.println("Mom's phone numbers:");
+        System.out.println("Номера телефонов для фамилии Петров:);
         for (String number : numbers2) {
             System.out.println(number);
         }
 
         System.out.println();
-        System.out.println("Brother's phone numbers:");
+        System.out.println("Номера телефонов для фамилии Малышев:");
         for (String number : numbers3) {
             System.out.println(number);
         }
