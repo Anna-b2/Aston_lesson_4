cfd562c8bdddb4303c72fb134fbfbe12effec2df
Anna-b2 <84246490+Anna-b2@users.noreply.github.com>
2024-06-27 22:26:46 +0300

    Update PostmanTest.java

--- src/test/PostmanTest.java
+++ src/test/PostmanTest.java
@@ -13,7 +13,7 @@ public class PostmanTest {
         RestAssured.baseURI = "https://postman-echo.com";
     }
     @Test
-    public void testGet() {
+    public void testRequest() {
         given()
                 .queryParam("foo1", "bar1")
                 .queryParam("foo2", "bar2")
@@ -21,51 +21,145 @@ public class PostmanTest {
                 .get("/get")
                 .then()
                 .statusCode(200)
-                .body("args.foo1", equalTo("bar1"))
-                .body("args.foo2", equalTo("bar2"));
+               .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"))
+                .body("headers.host",equalTo("postman-echo.com"))
+                .body("headers.x-request-start",notNullValue())
+                .body("headers.connection",equalTo("close"))
+                .body("headers.x-forwarded-proto",equalTo("https"))
+                .body("headers.x-forwarded-port",equalTo("443"))
+                .body("headers.x-amzn-trace-id",notNullValue())
+                .body("headers.user-agent",equalTo("Apache-HttpClient/4.5.3 (Java/21.0.2)"))
+                .body("headers.accept",equalTo("*/*"))
+                .body("headers.postman-token",nullValue())
+                .body("headers.accept-encoding",equalTo("gzip,deflate"))
+                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
     }
     @Test
-    public void testPost() {
-        given()
+    public void testPostRaw() {
+         given()
+                Response response = given()
+                .log().all()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.").
+        .when()
+                .post("https://postman-echo.com/post")
+       .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-request-start", notNullValue())
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.x-amzn-trace-id", notNullValue())
+                .body("cache-control", nullValue())
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
+                .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
+                .body("json", nullValue())
+                .body("url", equalTo("https://postman-echo.com/post"));
+    }
+    @Test
+    public void testPostData() {
+                given()
+                .log().all()
                 .contentType("application/json")
-                .body("{\"name\":\"Nikita\", \"lastName\":\"Kostyukov\"}")
                 .when()
                 .post("/post")
                 .then()
+                .log().all()
                 .statusCode(200)
-                .body("json.name", equalTo("Nikita"))
-                .body("json.lastName", equalTo("Kostyukov"));
+                .body("form.foo1", nullValue())
+                .body("form.foo2", nullValue())
+                .body("headers.host",equalTo("postman-echo.com"))
+                .body("headers.x-request-start",notNullValue())
+                .body("headers.connection",equalTo("close"))
+                .body("headers.connect-lenght",nullValue())
+                .body("headers.x-forwarded-proto",equalTo("https"))
+                .body("headers.x-forwarded-port",equalTo("443"))
+                .body("headers.x-amzn-trace-id",notNullValue())
+                .body("headers.user-agent",equalTo("Apache-HttpClient/4.5.3 (Java/21.0.2)"))
+                .body("headers.accept",equalTo("*/*"))
+                .body("headers.postman-token",nullValue())
+                .body("headers.accept-encoding",equalTo("gzip,deflate,br"))
+                .body("headers.cookie",nullValue())
+                .body("json.foo1", nullValue())
+                .body("json.foo2", nullValue())
+                .extract().response();
     }
     @Test
-    public void testPut() {
+    public void testPatchRequest() {
         given()
-                .contentType("application/json")
-                .body("{\"foo1\":\"bar1\", \"foo2\":\"bar2\"}")
-                .when()
-                .put("/put")
-                .then()
-                .statusCode(200)
-                .body("json.foo1", equalTo("bar1"))
-                .body("json.foo2", equalTo("bar2"));
-    }
-    @Test
-    public void testPatch() {
-        given()
-                .contentType("application/json")
-                .body("{\"foo1\":\"bar1\", \"foo2\":\"bar2\"}")
+                .log().all()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
                 .when()
                 .patch("/patch")
                 .then()
+                .log().all()
                 .statusCode(200)
-                .body("json.foo1", equalTo("bar1"))
-                .body("json.foo2", equalTo("bar2"));
+                .body("data", equalTo("This is expected to be sent back as part of response body."))
+                .body("headers.host",equalTo("postman-echo.com"))
+                .body("headers.x-request-start",notNullValue())
+                .body("headers.connection",equalTo("close"))
+                .body("headers.connect-lenght",nullValue())
+                .body("headers.x-forwarded-proto",equalTo("https"))
+                .body("headers.x-forwarded-port",equalTo("443"))
+                .body("headers.x-amzn-trace-id",notNullValue())
+                .body("headers.user-agent",equalTo("Apache-HttpClient/4.5.3 (Java/21.0.2)"))
+                .body("headers.accept",equalTo("*/*"))
+                .body("headers.postman-token",nullValue())
+                .body("headers.accept-encoding",equalTo("gzip,deflate,br"))
+                .body("headers.cookie",nullValue())
+                .extract().response();
     }
+ @Test
+    public void testPutRequest() {
+    given()
+                .log().all()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .put("/put")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."))
+                .body("headers.host",equalTo("postman-echo.com"))
+                .body("headers.x-request-start",notNullValue())
+                .body("headers.connection",equalTo("close"))
+                .body("headers.connect-lenght",nullValue())
+                .body("headers.x-forwarded-proto",equalTo("https"))
+                .body("headers.x-forwarded-port",equalTo("443"))
+                .body("headers.x-amzn-trace-id",notNullValue())
+                .body("headers.user-agent",equalTo("Apache-HttpClient/4.5.3 (Java/21.0.2)"))
+                .body("headers.accept",equalTo("*/*"))
+                .body("headers.postman-token",nullValue())
+                .body("headers.accept-encoding",equalTo("gzip,deflate,br"))
+                .body("headers.cookie",nullValue())
+                .extract().response();
     @Test
     public void testDelete() {
-        when()
+                given()
+                .log().all()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
                 .delete("/delete")
                 .then()
-                .statusCode(200)
-                .body("json", nullValue()); // тело ответа пустое, проверяем на null
-    }
+                .log().all()
+                .assertThat().statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-request-start", notNullValue())
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.x-amzn-trace-id", notNullValue())
+                .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
+                .body("headers.accept", equalTo("*/*"))
+                .body("cache-control", nullValue())
+                .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
+                .body("json", nullValue())
+                .body("url", equalTo("https://postman-echo.com/delete"));
 }
